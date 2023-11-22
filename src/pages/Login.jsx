7c56358ced8a984bd190ac58a6c077dd8397650b
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-11-23 00:49:28 +0530

    wfwf

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import { TextField } from "@mui/material";
+import axios from "axios";
+import { server } from "../App";
+import { Context } from "../App";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isAuthenticated, setIsAuthenticated ,isLoading,setIsLoading} = useContext(Context);
+  const submitHandler = async (e) => {
+    setIsLoading(true)
+    e.preventDefault();
+    if (email === "" || password === "") {
+      toast.error("Please Fill  Email, Password");
+      return;
+    }
+    try {
+      console.log(server);
+      const res = await axios.post(
+        `https://todobackend-aw6o.onrender.com/api/v1/users/login`,
+        { email, password },{withCredentials:true}
+      );
+      console.log(res, "res");
+      toast.success(res.data.message);
+
+      setEmail("");
+      setPassword("");
+      setIsAuthenticated(true);
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error, "error");
+      toast.error(error.response.data.message);
+      setIsAuthenticated(false);
+      setIsLoading(false)
+
+    }
+  };
+
+  if (isAuthenticated) return <Navigate to="/" />;
+
   return (
     <div className="min-h-screen flex-col flex items-center justify-start">
       <div className="max-w-[40%] w-full px-12 py-24 flex flex-col items-center justify-center ">
         <h1 className="text-3xl my-4 font-bold">LOGIN</h1>
         <div className="w-full flex flex-col space-y-4">
-          <form action="" className="flex flex-col space-y-4" method="post">
+          <form onSubmit={submitHandler} className="flex flex-col space-y-4">
             <TextField
               type="email"
               className="full"
               fullWidth
               label="Email"
               color="secondary"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <TextField
               type="password"
@@ -22,6 +62,8 @@ const Login = () => {
               fullWidth
               label="Password"
               color="secondary"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <button className="bg-black py-4 text-white w-full  rounded font-bold">
               LOGIN
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,39 +1,52 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import { TextField } from "@mui/material";
 import axios from "axios";
-import { server } from "../App";
+import { Context, server } from "../App";
 import { toast } from "react-hot-toast";
 
 const Register = () => {
-   
-    const [name,setName] = useState("")
-    const [email,setEmail]=useState("")
-    const [password,setPassword] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const {isAuthenticated,setIsAuthenticated,isLoading,setIsLoading} = useContext(Context)
 
-    const submitHandler = async (e) =>{
-        e.preventDefault()
-      try {
-        console.log(server)
-        const {data} =  await axios.post(`${server}/users/register`,{name,email,password},{
-            headers:{
-                "Content-Type":"application/json"
-            },
-            withCredentials:true
-           })
-    
-           toast.success(data.message)
-      } catch (error) {
-        console.log(error)
-        toast.error(error.message)
-      }
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+    if (name === "" || email === "" || password === "") {
+      toast.error("Please Fill Name, Email, Password");
+      return;
     }
+    try {
+      console.log(server);
+      const res = await axios.post(
+        `https://todobackend-aw6o.onrender.com/api/v1/users/register`,
+        { name, email, password },{withCredentials:true}
+      );
+      console.log(res, "res");
+      toast.success(res.data.message);
+      setName("");
+      setEmail("");
+      setPassword("");
+      setIsAuthenticated(true)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error, "error");
+      toast.error(error.response.data.message);
+      setIsAuthenticated(false)
+      setIsLoading(false)
+
+    }
+  };
+
+  if(isAuthenticated)return <Navigate to="/" />
   return (
     <div className="min-h-screen flex-col flex items-center justify-start">
       <div className="max-w-[40%] w-full px-12 py-24 flex flex-col items-center justify-center ">
         <h1 className="text-3xl my-4 uppercase font-bold">Register</h1>
         <div className="w-full flex flex-col space-y-4">
-          <form onSubmit={submitHandler} className="flex flex-col space-y-4" >
+          <form onSubmit={submitHandler} className="flex flex-col space-y-4">
             <TextField
               type="text"
               className="full"
@@ -41,7 +54,7 @@ const Register = () => {
               label="Name"
               color="secondary"
               value={name}
-              onChange={(e)=>setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
             />
 
             <TextField
@@ -51,7 +64,7 @@ const Register = () => {
               label="Email"
               color="secondary"
               value={email}
-              onChange={(e)=>setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <TextField
               type="password"
@@ -60,9 +73,9 @@ const Register = () => {
               label="Password"
               color="secondary"
               value={password}
-              onChange={(e)=>setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <button className="bg-black py-4 text-white w-full  rounded font-bold">
+            <button disabled={isLoading} className="bg-black py-4 text-white w-full  rounded font-bold">
               REGISTER
             </button>
           </form>
