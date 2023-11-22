cea84fbe33172c49793cb31fa10ea3bad2c26d9f
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-11-23 00:49:30 +0530

    wfwfw

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,67 @@ import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
-export const server = "https://todobackend-aw6o.onrender.com/api/v1"
+export const Context = createContext({
+  isAuthenticated: false,
+  setIsAuthenticated: () => {},
+  isLoading: false,
+  setIsLoading: () => {},
+  user:{},
+  setUser:()=>{}
+});
+
+const ConextProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [user,setUser] = useState({})
+  const value = {
+    isAuthenticated,
+    setIsAuthenticated,
+    isLoading,
+    setIsLoading,
+    user,
+    setUser
+  };
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+export const server = "https://todobackend-aw6o.onrender.com/api/v1";
 
 function App() {
+  const {user,setUser,setIsAuthenticated,isAuthenticated} = useContext(Context)
+  useEffect(() => {
+    axios
+      .get("https://todobackend-aw6o.onrender.com/api/v1/users/me",{withCredentials:true})
+      .then((res) => {
+        setIsAuthenticated(true)
+        setUser(res.data.user)
+       
+      }).catch((err)=>{
+        setIsAuthenticated(false)
+        setUser({})
+        
+      });
+  }, [isAuthenticated]);
+
+  console.log(user)
   return (
     <div>
-      {" "}
-      <Router>
-        <Header />
+      <ConextProvider>
+        <Router>
+          <Header />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        <Toaster/>
-      </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Toaster />
+        </Router>
+      </ConextProvider>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { Context } from "../App";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const Header = () => {
+  const { isAuthenticated, setIsAuthenticated, isLoading, setIsLoading } =
+    useContext(Context);
+  const logoutHandler = async () => {
+    setIsLoading(true)
+    try {
+      const { data } = await axios.get(
+        "https://todobackend-aw6o.onrender.com/api/v1/users/logout"
+      );
+      toast.success(data?.message);
+      setIsAuthenticated(false);
+      setIsLoading(false)
+    } catch (error) {
+      toast.error(error.message)
+      setIsLoading(false)
+
+    }
+  };
+
   return (
     <div className="w-full bg-black text-white py-4 px-4 flex items-center justify-between">
-      <Link to="/">  <h1 className="text-2xl font-bold">TODOs</h1></Link>
+      <Link to="/">
+        {" "}
+        <h1 className="text-2xl font-bold">TODOs</h1>
+      </Link>
       <nav className="flex space-x-12 text-white uppercase font-semibold text-md ">
         <NavLink
           to="/"
@@ -12,8 +36,22 @@ const Header = () => {
         >
           Home
         </NavLink>
-        <NavLink  className={({ isActive }) => (isActive ? "text-red-400" : "")} to="/profile">Profile</NavLink>
-        <NavLink  className={({ isActive }) => (isActive ? "text-red-400" : "")} to="/login">Login</NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-red-400" : "")}
+          to="/profile"
+        >
+          Profile
+        </NavLink>
+        {isAuthenticated ? (
+          <button disabled={isLoading} onClick={logoutHandler}>LOGOUT</button>
+        ) : (
+          <NavLink
+            className={({ isActive }) => (isActive ? "text-red-400" : "")}
+            to="/login"
+          >
+            Login
+          </NavLink>
+        )}
       </nav>
     </div>
   );
